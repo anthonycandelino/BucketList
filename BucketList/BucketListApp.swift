d4a87ab1208dc5b5637a587b622d2ad17b9a9856
Anthony Candelino <anthcandelino@gmail.com>
2024-09-12 21:43:48 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Anthony Candelino on 2024-09-12.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
